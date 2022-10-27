f8badad62b83e827a847c6d8edf6f412d0761c61
S554775 <pankajvshwkrm8@gmail.com>
2022-10-26 20:13:52 -0500

    updated computation.js with values

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var x = Math.random();
+var y = Math.random();
+
+var val1="Math.log(x) of value "+ x + " is "+ Math.log(x) 
+var val2="Math.imul(x, y) of value "+ x +"and"+ y +" is "+ Math.imul(x, y)
+var val3= "Math.log10(x) of value "+ x + " is "+ Math.log10(x);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var x = Math.random();
-  res.render('bonus', { title: `${Math.log(x)}` });
+  res.render('computation', { title:'Pankaj Sanjay Vishwakarma ',value1: val1, value2:val2, value3:val3 });
 });
 
 module.exports = router;
